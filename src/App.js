756cc7b6993960bea9f99668068b86254dc9f2d6
shenba Raghu <rshenba28:gmail.com>
2022-02-16 11:00:01 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Props from "./Props";
+import data from "./Data";
 
-function App() {
+// const allData = (values) => {
+//   return (
+//     <>
+//       <Props imgsrc={values.imgsrc} title={values.title} btn={values.btn} />
+//     </>
+//   );
+// };
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-5">
+        <div className="row">
+          {/* <Props
+            imgsrc={data[0].imgsrc}
+            title={data[0].title}
+            btn={data[0].btn}
+          /> */}
+          {/* {data.map(allData)} */}
+          {data.map((values) => {
+            return (
+              <>
+                <Props
+                  key={values.id}
+                  id={values.id}
+                  imgsrc={values.imgsrc}
+                  title={values.title}
+                  btn={values.btn}
+                />
+                
+              </>
+            );
+          })}
+
+          {/* <Props
+            imgsrc={data[1].imgsrc}
+            title={data[1].title}
+            btn={data[1].btn}
+          />
+          <Props
+            imgsrc={data[2].imgsrc}
+            title={data[2].title}
+            btn={data[2].btn}
+          />
+          <Props
+            imgsrc={data[3].imgsrc}
+            title={data[3].title}
+            btn={data[3].btn}
+          /> */}
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    imgsrc: "../images/img1.jpg",
+    title: "Model 1",
+    btn: "Know More",
+  },
+  {
+    id: 2,
+    imgsrc: "../images/img2.jpg",
+    title: "Model 2",
+    btn: "Know More",
+  },
+
+  {
+    id: 3,
+    imgsrc: "../images/img3.jpg",
+    title: "Model 3",
+    btn: "Know More",
+  },
+  {
+    id: 4,
+    imgsrc: "../images/img4.jpg",
+    title: "Model 4",
+    btn: "Know More",
+  },
+  {
+    id: 5,
+    imgsrc: "../images/img5.jpg",
+    title: "Model 5",
+    btn: "Know More",
+  },
+];
+export default data;
